32cace64e4430d7266048b928db36606738b3dd9
dr-rompecabezas <felavid@gmail.com>
2024-08-26 10:39:02 -0400

    chore: Update mypy and stubs dependencies and fix hint errors

--- pdc_ams/apps/users/views.py
+++ pdc_ams/apps/users/views.py
@@ -28,7 +28,7 @@ class UserUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
         assert self.request.user.is_authenticated
         return self.request.user.get_absolute_url()
 
-    def get_object(self):
+    def get_object(self, queryset=None):
         return self.request.user
 
 
