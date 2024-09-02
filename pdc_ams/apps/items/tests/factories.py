7aba995a0a726a03edce65aa27f28934e58378ce
dr-rompecabezas <felavid@gmail.com>
2024-09-01 20:52:30 -0400

    chore: Add factories for Question, Choice, and Item models

---
+++ pdc_ams/apps/items/tests/factories.py
@@ -0,0 +1,39 @@
+import factory
+from factory.django import DjangoModelFactory
+
+from pdc_ams.apps.blueprint.tests.factories import LearningObjectiveFactory
+from pdc_ams.apps.items.models import Choice
+from pdc_ams.apps.items.models import Item
+from pdc_ams.apps.items.models import Question
+from pdc_ams.apps.users.tests.factories import UserFactory
+
+
+class QuestionFactory(DjangoModelFactory):
+    class Meta:
+        model = Question
+
+    text = factory.Faker("sentence", nb_words=10)
+    type = Question.Type.STANDARD
+
+
+class ChoiceFactory(DjangoModelFactory):
+    class Meta:
+        model = Choice
+
+    text = factory.Faker("sentence", nb_words=5)
+    question = factory.SubFactory(QuestionFactory)
+    is_correct = False
+
+
+class ItemFactory(DjangoModelFactory):
+    class Meta:
+        model = Item
+
+    stem = factory.SubFactory(QuestionFactory)
+    status = Item.Status.DRAFT
+    rationale = factory.Faker("paragraph")
+    learning_objective = factory.SubFactory(LearningObjectiveFactory)
+    author = factory.SubFactory(UserFactory)
+    estimated_p_value = 0.85
+    p_value = None
+    legacy_id = None
