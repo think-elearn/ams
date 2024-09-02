6617dcc18cb96ad3aa63b17fd9435105f882245f
dr-rompecabezas <felavid@gmail.com>
2024-09-01 20:59:52 -0400

    Refactor models' string methods and add corresponding tests

--- pdc_ams/apps/items/tests/test_models.py
+++ pdc_ams/apps/items/tests/test_models.py
@@ -8,31 +8,34 @@ from .factories import QuestionFactory
 
 
 @pytest.mark.django_db
-def test_question_str():
-    question = QuestionFactory(
-        text="This is a test question that is quite long and should be truncated.",
-    )
-    assert str(question) == "This is a test question that is quite long"
+class TestQuestionModel:
+    def test_str_method(self):
+        question = QuestionFactory(
+            text="This is a test question that is quite long and should be truncated.",
+        )
+        assert str(question) == "This is a test question that is quite long"
 
 
 @pytest.mark.django_db
-def test_choice_str():
-    question = QuestionFactory(text="Sample question")
-    choice = ChoiceFactory(
-        text="This is a test choice that is quite long and should be truncated.",
-        question=question,
-    )
-    assert str(choice) == "This is a test choice"
+class TestChoiceModel:
+    def test_str_method(self):
+        question = QuestionFactory(text="Sample question")
+        choice = ChoiceFactory(
+            text="This is a test choice that is quite long and should be truncated.",
+            question=question,
+        )
+        assert str(choice) == "This is a test choice"
 
 
 @pytest.mark.django_db
-def test_item_str(user):
-    learning_objective = LearningObjectiveFactory()
-    question = QuestionFactory(text="This is a test question for item")
-    item = ItemFactory(
-        stem=question,
-        rationale="Sample rationale",
-        learning_objective=learning_objective,
-        author=user,
-    )
-    assert str(item) == "This is a test question for item"
+class TestItemModel:
+    def test_str_method(self, user):
+        learning_objective = LearningObjectiveFactory()
+        question = QuestionFactory(text="This is a test question for item")
+        item = ItemFactory(
+            stem=question,
+            rationale="Sample rationale",
+            learning_objective=learning_objective,
+            author=user,
+        )
+        assert str(item) == "This is a test question for item"
